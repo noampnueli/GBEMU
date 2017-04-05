2348fee8005695349b017172022ea7debadf09af
Shahar Rose <rose.shahar@gmail.com>
2017-04-05 18:46:00 +0300

    cpu registers and some refactor

--- CPU.h
+++ CPU.h
@@ -1,12 +1,25 @@
-//
-// Created by shahar on 4/5/17.
-//
+#include "definitions.h"
 
 #ifndef GBEMU_CPU_H
 #define GBEMU_CPU_H
 
+class Z80
+{
+public:
+    typedef struct clock
+    {
+        byte m, t;
+    } clock;
 
-class CPU {
+    typedef struct regs
+    {
+        byte a, b, c, d, e, h, l, f; // 8-bit registers
+        word pc, sp; // 16-bit registers
+        byte m, t;
+    } r;
+
+    regs _r = {}; // init struct members to 0
+    clock clock = {};
 
 };
 
