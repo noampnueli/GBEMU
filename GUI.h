8f370feca5a85c17f40c8b44f9078a4c19c9bd3d
Noam <noam.pnueli@gmail.com>
2017-04-05 23:19:45 +0300

    added sdl to link flags + added GUI

---
+++ GUI.h
@@ -0,0 +1,28 @@
+//
+// Created by bubz on 4/5/17.
+//
+
+#ifndef GBEMU_GUI_H
+#define GBEMU_GUI_H
+
+#include <SDL2/SDL.h>
+
+SDL_Window* window;
+SDL_Surface* surface;
+SDL_Renderer* renderer;
+
+void create_window(const int width, const int height)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("GBEMU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              width, height, SDL_WINDOW_SHOWN);
+
+    surface = SDL_GetWindowSurface(window);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+}
+
+#endif //GBEMU_GUI_H
