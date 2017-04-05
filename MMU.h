4f5cbf23537b61330bda8db3aa3147753800a8d9
Noam <noam.pnueli@gmail.com>
2017-04-05 19:50:20 +0300

    added rb and rw

--- MMU.h
+++ MMU.h
@@ -5,8 +5,19 @@
 #ifndef GBEMU_MMU_H
 #define GBEMU_MMU_H
 
-char memory[0xFFFF];
+#include "definitions.h"
 
+byte memory[0xFFFF];
+
+byte read_byte(word addr)
+{
+    return memory[addr];
+}
+
+word read_word(word addr)
+{
+    return (word) memory[addr] + (word) (memory[addr + 1] << 8);
+}
 
 
 #endif //GBEMU_MMU_H
