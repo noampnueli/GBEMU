8b706bb7d06f04d7b0402d85a955462bba246b97
Shahar Rose <rose.shahar@gmail.com>
2017-04-05 20:17:27 +0300

    added flags to Z80 cpu

--- CPU.h
+++ CPU.h
@@ -19,7 +19,51 @@ public:
     } r;
 
     regs _r = {}; // init struct members to 0
-    clock clock = {};
+    clock _clock = {};
+
+    bool isZero()
+    {
+        return (_r.f & 0x80) > 0; // remove non-related bits, check if zero is on
+    }
+
+    void setZero(bool state)
+    {
+        if(isZero() != state) // if the desired state isn't the current, flip the flag value
+            _r.f = _r.f ^ 0x80;
+    }
+
+    bool isOperation()
+    {
+        return (_r.f & 0x40) > 0;
+    }
+
+    void setOperation(bool state)
+    {
+        if(isOperation() != state)
+            _r.f = _r.f ^ 0x40;
+    }
+
+    bool isHalfCarry()
+    {
+        return (_r.f & 0x20) > 0;
+    }
+
+    void setHalfCarry(bool state)
+    {
+        if(isHalfCarry() != state)
+            _r.f = _r.f ^ 0x20;
+    }
+
+    bool isCarry()
+    {
+        return (_r.f & 0x10) > 0;
+    }
+
+    void setCarry(bool state)
+    {
+        if(isCarry() != state)
+            _r.f = _r.f ^ 0x10;
+    }
 
 };
 
