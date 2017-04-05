7ba3bc720e70001612badbcd8a6cef28c569d7da
Shahar Rose <rose.shahar@gmail.com>
2017-04-05 18:24:20 +0300

    CPU file

---
+++ CPU.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shahar on 4/5/17.
+//
+
+#include "CPU.h"
---
+++ CPU.h
@@ -0,0 +1,14 @@
+//
+// Created by shahar on 4/5/17.
+//
+
+#ifndef GBEMU_CPU_H
+#define GBEMU_CPU_H
+
+
+class CPU {
+
+};
+
+
+#endif //GBEMU_CPU_H
