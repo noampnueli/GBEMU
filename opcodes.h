c3ff4991a82786ea9627492494fdd8e020240db3
Noam <noam.pnueli@gmail.com>
2017-04-06 23:38:47 +0300

    a few opcodes

--- opcodes.h
+++ opcodes.h
@@ -13,4 +13,23 @@ void opcode_0x00()
     }
 }
 
+void opcode_0x01(Z80& cpu)
+{
+    cpu._r.c = read_byte(cpu._r.pc);
+    cpu._r.b = read_byte((word) (cpu._r.pc + 1));
+    cpu._r.pc += 2;
+    cpu._r.m = 3;
+}
+
+void opcode_0x02(Z80& cpu)
+{
+    write_byte(cpu._r.a, (word) ((cpu._r.b << 8) + cpu._r.c));
+    cpu._r.m = 2;
+}
+
+void opcode_0x03(Z80& cpu)
+{
+    
+}
+
 #endif //GBEMU_OPCODES_H
