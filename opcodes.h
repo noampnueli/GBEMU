b4394c3d5a8274f4b9d53c2a70781b0b0c8a8b8b
Noam <noam.pnueli@gmail.com>
2017-04-07 12:40:33 +0300

    opcode 0x08

--- opcodes.h
+++ opcodes.h
@@ -64,6 +64,14 @@ void opcode_0x07(Z80& cpu)
     cpu._r.a = (cpu._r.a << 1) + carry;
     if(cpu._r.a == 0)
         cpu.set_zero(1);
+    cpu._r.m = 1;
+}
+
+void opcode_0x08(Z80& cpu)
+{
+    write_word(cpu._r.sp, cpu._r.pc);
+    cpu._r.pc += 2;
+    cpu._r.m = 3;
 }
 
 void opcode_0x10()
