4115f1a9389b0a909f982e5ea10bd055daac15d5
Noam <noam.pnueli@gmail.com>
2017-04-12 20:57:13 +0300

    eugh

--- main.cpp
+++ main.cpp
@@ -47,9 +47,8 @@ void dispatcher()
             opmap[op](cpu);
         }
 
-        if(cpu._r.pc < 0x100)
-            printf("OPCODE: %x PC: %x DE: %x%x B: %x HL: %x%x\n", op, old_pc - 1, cpu._r.d, cpu._r.e, cpu._r.b,
-                   cpu._r.h, cpu._r.l);
+        printf("OPCODE: %x PC: %x DE: %x%x B: %x HL: %x%x\n", op, old_pc - 1, cpu._r.d, cpu._r.e, cpu._r.b,
+               cpu._r.h, cpu._r.l);
 
 //        print_registers();
         cpu._clock.m += cpu._r.m; // add time to CPU clock
